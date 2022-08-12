f8b10a7f3c026812ade3180cb18c950e6e0bb62b
username <youremail@domain.com>
2022-08-12 16:37:47 -0400

    user model

--- models/user.js
+++ models/user.js
@@ -1,3 +1,14 @@
 const mongoose = require('mongoose');
 
 // Create your User Model
+const userSchema = new mongoose.Schema({
+    userName: String,
+    password: String,
+    location: String,
+    goal: String,
+    bio: String,
+}, {
+    timestamps: true
+});
+      
+module.exports = User;
