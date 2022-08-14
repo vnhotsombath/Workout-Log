f57968d6f72c264a255a322a90a9ba736e808319
username <youremail@domain.com>
2022-08-14 18:29:08 -0400

    created new routinecontroller

--- routes/routines.js
+++ routes/routines.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const routineController = require('../controllers/routines');
 
-router.get('/routines/new, routineController.new');
+router.get('/routines/new', routineController.new);
 router.post('/routines', routineController.create);
 
 module.exports = router;
