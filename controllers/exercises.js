77602ec34cb2717a75459bc63c2a949f3559917f
username <youremail@domain.com>
2022-08-16 19:43:21 -0400

    current exercises

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -1,18 +1,16 @@
 const Exercise = require('../models/excerise');
-const routine = require('../models/routine');
+const Routine = require('../models/routine');
 
 module.exports = {
     show
 }
 
 function show (req, res){
-    Routine.findById(req.params.id).then((routine)) => {
+    Routine.findById(req.params.id)
         Exercise.find({ routineId: routine})
         .then(exercise => {
-            res.render(`exercise/show`, { exercise, routine, user})
+            res.render(`exercise/show`, { Exercise, Routine, User})
         })
-        .catch(err) => {
-            next(err)
+        .catch(err) 
+        next(err)
         }
-    }
-}
